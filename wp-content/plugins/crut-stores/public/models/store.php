60bbe8a77e1fc07e62ada2bbf554869075fd4f57
ocetot <perisjara@gmail.com>
2023-03-16 03:20:27 -0300

    algo del video 11

--- wp-content/plugins/crut-stores/public/models/store.php
+++ wp-content/plugins/crut-stores/public/models/store.php
@@ -3,25 +3,25 @@
 
 class Store{
 
-  /*   public function index()
+    public function index()
     {
         global $wpdb;
-        $table_name = $wpdb->prefix . "stores"; 
+        $table_name = $wpdb->prefix . "store"; 
         $wpdb->get_results('SELECT * FROM '.$table_name . ' WHERE deleted_at IS NULL', OBJECT );
         $response = $wpdb->last_result;
         return $response;
-    } */
+    }
 
-   /*  public function save($store_info)
+    public function save($store_info)
     {
         global $wpdb;
-        $table_name = $wpdb->prefix . "stores";
+        $table_name = $wpdb->prefix . "store";
         $wpdb->insert($table_name, $store_info);
         $response = $wpdb;
         return $this->show($response->insert_id);
        
-    } */
-    public function save($store_info)
+    }
+    /* public function save($store_info)
     {
         global $wpdb;
         $table_name = $wpdb->prefix."store";
@@ -30,13 +30,13 @@ class Store{
         $wpdb->close();
         return $response;
        
-    } 
+    }  */
 
 
-   /*  public function update($store_info, $id)
+    public function update($store_info, $id)
     {
         global $wpdb;
-        $table_name = $wpdb->prefix . "stores"; 
+        $table_name = $wpdb->prefix . "store"; 
         $wpdb->update($table_name,$store_info,array('id'=>$id));
         $response = $wpdb;
         return $response;
@@ -45,7 +45,7 @@ class Store{
     public function show($id)
     {
        global $wpdb;
-       $table_name = $wpdb->prefix . "stores"; 
+       $table_name = $wpdb->prefix . "store"; 
        $response = $wpdb->get_row("SELECT * FROM $table_name WHERE id = $id" ,OBJECT, 0);
        return $response;
 
@@ -54,11 +54,11 @@ class Store{
     public function destroy($id)
     {
       global $wpdb;
-      $table_name = $wpdb->prefix . "stores"; 
+      $table_name = $wpdb->prefix . "store"; 
       $wpdb->delete($table_name, array('id'=>$id));
       $response = $wpdb->rows_affected == 1 ? $id : 0 ;
       return $response;
     }
- */
+
 }
 ?>
